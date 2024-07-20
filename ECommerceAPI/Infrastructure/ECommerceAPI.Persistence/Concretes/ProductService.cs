e737c1a7da538f0b0bb1d97413a232b955a9bdfb
Baris TAS <baristtas@gmail.com>
2024-07-21 01:54:30 +0300

    Backend yapı oluşturuldu.

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,58 @@
+﻿using ECommerceAPI.Application.Abstractions;
+using ECommerceAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Concretes
+{
+    internal class ProductService : IProductService
+    {
+        public List<Product> GetProducts() => new()
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product1",
+                Stock = 100,
+                CreatedDate = DateTime.Now,
+                Price = 100
+            },
+           new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product2",
+                Stock = 100,
+                CreatedDate = DateTime.Now,
+                Price = 22
+           },
+           new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product3",
+                Stock = 100,
+                CreatedDate = DateTime.Now,
+                Price = 55
+           },
+           new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product4",
+                Stock = 100,
+                CreatedDate = DateTime.Now,
+                Price = 90
+           },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product5",
+                Stock = 100,
+                CreatedDate = DateTime.Now,
+                Price = 80
+            },
+        };
+
+    }
+}
---
+++ ECommerceAPI/Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService m_productService;
+
+        public ProductsController(IProductService productService)
+        {
+            m_productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = m_productService.GetProducts();
+            return Ok(products);
+        }
+    }
+}
