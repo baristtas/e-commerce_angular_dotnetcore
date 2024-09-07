f179aa5df8618ac669c864b660c9daa66f925a5f
Baris TAS <baristtas@gmail.com>
2024-09-08 01:40:37 +0300

    front end komponentler modüller ekleniyor

---
+++ ecommerceFrontEnd/src/app/admin/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ComponentsModule } from './components/components.module';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
